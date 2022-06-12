a0183893e6004fc1811e632c6abda313adffbdaf
geust <muath.ye@gmail.com>
2022-06-11 19:56:11 -0700

    Update index.html

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,6 +11,7 @@ class WebhookController extends Controller
     public function receivedTextMessage()
     {
         Log::info(\request()->all());
+        return response()->json('', 200);
         return response()->json(\request()->hub_challenge, 200);
         return \request()->all()['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
         return \request()->all()['entry'][0]['changes'][0]['value']['contacts'];
