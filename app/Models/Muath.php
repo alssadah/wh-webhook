172b922ab19fd75e484e8d41f99af758ad1fc21f
geust <muath.ye@gmail.com>
2022-06-11 18:45:09 -0700

    Update index.html

---
+++ app/Models/Muath.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Muath extends Model
+{
+    // Hi Alaa
+    use HasFactory;
+}
