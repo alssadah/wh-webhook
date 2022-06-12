5646e1e2c62a2ec7151d38c8a30915be0e5db47a
geust <muath.ye@gmail.com>
2022-06-11 19:43:55 -0700

    Update index.html

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,7 +11,8 @@ class WebhookController extends Controller
     public function receivedTextMessage()
     {
         Log::info(\request()->all());
-        return response()->json('AlaToken');
+        Log::info(\request()->hub_challenge);
+        return response()->json(\request()->hub_challenge, 200);
         return \request()->all()['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
         return \request()->all()['entry'][0]['changes'][0]['value']['contacts'];
 
