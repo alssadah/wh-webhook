9a9babb3f058014e19252abe4c75562034fccf34
geust <muath.ye@gmail.com>
2022-06-11 20:02:58 -0700

    Update index.html

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('webhook', [\App\Http\Controllers\WebhookController::class, 'receivedTextMessage']);
+//
+//Route::get('webhook', [\App\Http\Controllers\WebhookController::class, 'receivedTextMessage']);
