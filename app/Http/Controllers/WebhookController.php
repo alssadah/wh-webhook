3b8755f1273c10923b95d4481da7cde8ca9e5bd2
geust <muath.ye@gmail.com>
2022-06-11 19:07:36 -0700

    Update index.html

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
     public function receivedTextMessage()
     {
+        Log::info(\request()->all());
         return response()->json('AlaToken');
         return \request()->all()['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
         return \request()->all()['entry'][0]['changes'][0]['value']['contacts'];
